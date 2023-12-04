55d188fa5880c7bcedac7e13ae4e9541faa8140e
Anastasia <nastushadem@mail.ru>
2023-12-04 18:20:33 +0500

    add Example 005 ConditionfElse

---
+++ Example005_ConditionfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anastasia")
+{
+    Console.WriteLine("Ура, это же НАСТЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
